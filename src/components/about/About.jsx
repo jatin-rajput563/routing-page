7db350aea7af95527720731e0bc65aa6e26fe155
jatin-rajput563 <jkt3298@gmail.com>
2025-05-16 12:19:43 +0530

    starting-about-section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import CommonHeading from '../common/CommonHeading'
+import CommonPara from '../common/CommonPara'
+import CommonButton from '../common/CommonButton'
+import aboutImg from '../../assets/images/png/about-main-img.png'
 
 const About = () => {
     return (
         <>
-            <div className="min-h-screen"></div>
+            <div className="min-h-screen py-[100px]">
+                <div className="max-w-[1140px] mx-auto ">
+                    <div className="flex flex-row flex-wrap">
+                        <div className="min-lg:w-6/12">
+                            <CommonHeading HeadingText="About Us" />
+                            <CommonPara ParaClass="max-w-[558px] !opacity-80 pt-[16px] pb-[32px]" ParaText="At Leeds Junk, we believe waste removal should be simple, affordable, and eco-friendly. Whether you're clearing out your home, office, or construction site, we provide a hassle-free solution tailored to your needs. With a dedicated team and a commitment to responsible disposal, we ensure that as much waste as possible is recycled or repurposed, reducing landfill impact." />
+                            <CommonButton />
+                        </div>
+                        <div className="min-lg:w-6/12">
+                            <img className='pl-[50px] max-w-[546px] w-full' src={aboutImg} alt="about-img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/common/CommonHeading.jsx
+++ src/components/common/CommonHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CommonHeading = ({ HeadingText, HeadingClass }) => {
     return (
         <>
-            <h2 className={`font-roboto text-semibold text-[48px] leading-[120%] ${HeadingClass}`}>{HeadingText}</h2>
+            <h2 className={`font-roboto font-semibold text-[48px] leading-[120%] ${HeadingClass}`}>{HeadingText}</h2>
         </>
     )
 }
