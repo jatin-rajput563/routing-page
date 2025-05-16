24e7af772c3d2c55d605deaa4ee87932a8fdc539
jatin-rajput563 <jkt3298@gmail.com>
2025-05-16 12:45:22 +0530

    starting-contact-section

--- src/components/common/CommonInput.jsx
+++ src/components/common/CommonInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const CommonInput = ({type, placeholder}) => {
+const CommonInput = ({ type, placeholder, InputClass }) => {
     return (
         <>
-            <input type={type} placeholder={placeholder} />
+            <input type={type} placeholder={placeholder} className={`bg-white rounded-[20px] h-[36px] w-full border-0 outline-0 pl-3 ${InputClass}`} />
         </>
     )
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,9 +10,15 @@ const Contact = () => {
                 <div className="max-w-[1140px] mx-auto">
                     <CommonHeading HeadingText="Ways to Get in Touch" HeadingClass="text-center" />
                     <div className='max-w-[662px] p-[16px] bg-[linear-gradient(94.55deg,_rgba(199,255,81,0.2)_-14.37%,_rgba(0,201,52,0.2)_49.67%,_rgba(81,255,192,0.2)_111.65%)] border border-solid [border-image-source:linear-gradient(94.55deg,_rgba(199,255,81,0.4)_-14.37%,_rgba(0,201,52,0.4)_49.67%,_rgba(81,255,192,0.4)_111.65%)] [border-image-slice:1] mx-auto flex flex-col'>
-                        <div>
-                            <CommonPara ParaText="First name" ParaClass="opacity-80 !leading-[160%]" />
-                            <CommonInput />
+                        <div className='flex justify-between'>
+                            <div>
+                                <CommonPara ParaText="First name" ParaClass="opacity-80 !leading-[160%]" />
+                                <CommonInput />
+                            </div>
+                            <div>
+                                <CommonPara ParaText="First name" ParaClass="opacity-80 !leading-[160%]" />
+                                <CommonInput />
+                            </div>
                         </div>
                     </div>
                 </div>
