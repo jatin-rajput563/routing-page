b401731dc89de5999a01a423e137bf118a385df3
jatin-rajput563 <jkt3298@gmail.com>
2025-05-16 09:58:28 +0530

    adding-common-para

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import CommonPara from './common/CommonPara'
 
 const Header = () => {
     return (
@@ -7,8 +8,10 @@ const Header = () => {
             <div className="min-h-screen bg-[url(../src/assets/images/png/bg-image.png)] bg-cover bg-no-repeat bg-center">
                 <div className="w-full bg-black ">
                     <div className="max-w-[1140px] items-center flex h-[50px] justify-between w-full mx-auto">
-                        <p className='text-white '>Leeds junk are a local waste collection/removal business.</p>
-                        <p className='text-white'>Eng</p>
+
+                        <CommonPara ParaClass="!text-white !text-[12px]" ParaText="Leeds junk are a local waste collection/removal business." />
+                        <CommonPara ParaClass="!text-white !text-[12px]" ParaText="Eng" />
+
                     </div>
                 </div>
                 <Nav />
--- src/components/common/CommonPara.jsx
+++ src/components/common/CommonPara.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const CommonPara = () => {
+const CommonPara = ({ ParaText, ParaClass }) => {
     return (
         <>
-            <p className='font-popins'></p>
+            <p className={`font-poppins text-black text-[16px] font-normal leading-[150%] ${ParaClass}`}>{ParaText}</p>
         </>
     )
 }
